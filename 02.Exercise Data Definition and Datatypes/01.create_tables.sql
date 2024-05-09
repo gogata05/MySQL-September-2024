6f8f958a2623507dbea42a1c11541242949d5ad3
gogata05 <33290584+gogata05@users.noreply.github.com>
2024-05-09 09:32:47 +0300

    create_tables

---
+++ 02.Exercise Data Definition and Datatypes/01.create_tables.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE `minions`;
+
+USE `minions`;
+
+CREATE TABLE `minions` (
+	`id` INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(255),
+    `age` VARCHAR(255)
+);
+
+CREATE TABLE `towns` (
+	`town_id`INT PRIMARY KEY AUTO_INCREMENT,
+    `name` VARCHAR(255)
+);
+
+
+	
+
+
