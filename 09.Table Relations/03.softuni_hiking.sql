2020a49946961cee7296386d90a6669cc0a22915
gogata05 <33290584+gogata05@users.noreply.github.com>
2024-05-13 11:53:14 +0300

    softuni_hiking

---
+++ 09.Table Relations/03.softuni_hiking.sql
@@ -0,0 +1,8 @@
+SELECT 
+	`starting_point`,
+    `end_point`,
+    `leader_id`,
+    CONCAT(`first_name`, ' ', `last_name`) AS `leader_name`
+ FROM `routes` AS `r`
+ JOIN `campers` AS `c`
+	ON r.leader_id = c.id
