8b3c0ca02b88bf1ffbca25ce1b8bd0d18a474f4b
gogata05 <33290584+gogata05@users.noreply.github.com>
2024-05-15 04:07:40 +0300

    employees_promotion

---
+++ 13.Database Programmability and Transactions/02.employees_promotion.sql
@@ -0,0 +1,14 @@
+DELIMITER %%
+
+CREATE PROCEDURE usp_raise_salaries(`department_name` VARCHAR(50))
+BEGIN
+
+	UPDATE `employees` AS e
+    JOIN `departments` AS d
+		USING(`department_id`) 
+    SET `salary` = `salary` * 1.05
+    WHERE d.`name` = `department_name`;
+    
+END%%
+
+CALL `soft_uni`.`usp_raise_salaries`('Finance');
