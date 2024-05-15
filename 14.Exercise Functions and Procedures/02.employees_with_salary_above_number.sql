2dc6c49a1f46f332a6ac8bccf2c282938286afcd
gogata05 <33290584+gogata05@users.noreply.github.com>
2024-05-15 21:26:56 +0300

    employees_with_salary_above_number

---
+++ 14.Exercise Functions and Procedures/02.employees_with_salary_above_number.sql
@@ -0,0 +1,10 @@
+DELIMITER %%
+
+CREATE PROCEDURE usp_get_employees_salary_above(current_number DECIMAL(19, 4))
+BEGIN
+
+		SELECT `first_name`, `last_name` FROM `employees`
+        WHERE `salary` >= current_number
+        ORDER BY `first_name`, `last_name`, `employee_id`;
+
+END%%
