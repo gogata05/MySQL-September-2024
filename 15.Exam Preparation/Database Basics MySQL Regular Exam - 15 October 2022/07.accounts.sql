3eb612c76da9041d1f657976261354075d681655
gogata05 <33290584+gogata05@users.noreply.github.com>
2024-05-24 01:32:33 +0300

    accounts

---
+++ 15.Exam Preparation/Database Basics MySQL Regular Exam - 15 October 2022/07.accounts.sql
@@ -0,0 +1,6 @@
+SELECT
+	CONCAT(`last_name`, `first_name`, CHAR_LENGTH(`first_name`), 'Restaurant') AS `username`,
+    REVERSE(SUBSTR(`email`, 2, 12)) AS `password`
+FROM `waiters`
+WHERE `salary` IS NOT NULL
+ORDER BY `password` DESC
