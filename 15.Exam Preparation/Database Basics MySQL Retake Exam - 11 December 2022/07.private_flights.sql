a30a56039937583745834e8266b22e0addf0e793
gogata05 <33290584+gogata05@users.noreply.github.com>
2024-06-03 02:46:49 +0300

    private_flights

---
+++ 15.Exam Preparation/Database Basics MySQL Retake Exam - 11 December 2022/07.private_flights.sql
@@ -0,0 +1,7 @@
+SELECT 
+	CONCAT(UPPER(LEFT(`last_name`, 2)), `country_id`) AS 'flight_code',
+	CONCAT(`first_name`, ' ', `last_name`) AS 'full_name',
+	`country_id`
+FROM `passengers`
+WHERE `id` NOT IN (SELECT `passenger_id` FROM `flights_passengers`)
+ORDER BY `country_id`
